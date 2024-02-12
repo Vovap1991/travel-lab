700e8c5a75685eb89409ea6902a00d1871f7bdb7
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2024-02-12 16:12:00 +0200

    header styles delet NavLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import { Contacts } from 'components/Contacts/Contacts';
 import { Navigation } from 'components/Navigation/Navigation';
 import { SocialMedia } from 'components/SocialMedia/SocialMedia';
-import { NavLink } from 'react-router-dom';
 import { HeaderContainer, HeaderWrapper, Logo } from './Header.styled';
 
 export const Header = () => {
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,3 @@
-import { NavLink } from 'react-router-dom';
 import { NavigationList, LinkNavigation } from './Navigation.styled';
 
 export const Navigation = () => {
