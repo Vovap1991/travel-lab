b914b25c32c220994c02934d5be62f7b0160d044
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2024-03-19 19:25:18 +0200

    finished about ua page

--- src/pages/AboutCompanyPage/AboutCompanyPage.js
+++ src/pages/AboutCompanyPage/AboutCompanyPage.js
@@ -1,5 +1,53 @@
+import { Advantages } from 'components/Advantages/Advantages';
+import {
+  AboutPageWrapper,
+  AboutPageTitle,
+  AboutPageText,
+  AboutPageTextWrapper,
+  AboutPageContainer,
+} from './AboutCompanyPage.styled';
+
 const AboutCompanyPage = () => {
-  return <div>This is AboutCompanyPage</div>;
+  return (
+    <AboutPageWrapper>
+      <AboutPageContainer>
+        <AboutPageTitle>Про Travel.LAB</AboutPageTitle>
+        <AboutPageTextWrapper>
+          <AboutPageText>
+            Запрошуємо вас на захоплюючу подорож через найкрасивіші країни та
+            екзотичні куточки нашої планети разом з нами! Наші двері відчинені
+            для тих, хто мріє відкрити світ та насолодитися неповторними
+            моментами подорожей.
+          </AboutPageText>
+          <AboutPageText>
+            За роки роботи ми накопичили вражаючий досвід та збільшили свої
+            знання в галузі туризму, щоб забезпечити вам найкращі умови для
+            відпочинку. Наша команда - це колектив досвідчених професіоналів,
+            які докладають всіх зусиль, щоб кожна ваша подорож була
+            найяскравішою та незабутньою.
+          </AboutPageText>
+          <AboutPageText>
+            У нашому розмаїтому портфелі ви знайдете тури на будь-який смак та
+            бюджет. Від розкішних відпочинків на білосніжних пляжах до
+            захоплюючих відкриттів у старовинних містах - ми маємо все необхідне
+            для створення вашої мрії про подорож.
+          </AboutPageText>
+          <AboutPageText>
+            Ми не лише пропонуємо тури, ми створюємо відмінний сервіс, який вас
+            приємно здивує. Ми готові вислухати ваші побажання та індивідуальні
+            запити, щоб зробити вашу подорож найкращою та найкомфортнішою.
+          </AboutPageText>
+          <AboutPageText>
+            Не вагайтесь вибирати нас, адже ми готові зробити все, щоб ваша
+            подорож стала незабутньою та наповненою враженнями. Разом з нами ви
+            отримаєте можливість насолодитися кожною миттю вашого подорожного
+            шляху!
+          </AboutPageText>
+        </AboutPageTextWrapper>
+      </AboutPageContainer>
+      <Advantages />
+    </AboutPageWrapper>
+  );
 };
 
 export default AboutCompanyPage;
--- src/pages/AboutCompanyPage/AboutCompanyPage.styled.js
+++ src/pages/AboutCompanyPage/AboutCompanyPage.styled.js
@@ -1,1 +1,33 @@
 import styled from 'styled-components';
+import { Container } from 'components/StyledComponents/Container';
+
+export const AboutPageContainer = styled(Container)`
+  width: 60%;
+`;
+
+export const AboutPageWrapper = styled.div`
+  background-color: var(--white);
+  padding-top: 50px;
+  padding-bottom: 50px;
+`;
+
+export const AboutPageTitle = styled.h3`
+  color: var(--black-primary);
+  display: flex;
+  justify-content: center;
+  font-size: 35px;
+  line-height: 35px;
+  margin-bottom: 50px;
+`;
+
+export const AboutPageText = styled.p`
+  color: var(--black-primary);
+  font-size: 20px;
+`;
+
+export const AboutPageTextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  justify-content: center;
+`;
