d1b87592a3b60beec9fe3b965107c795dbb29d5a
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2024-08-07 19:25:27 +0300

    delete our services

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,7 @@
 import { Advantages } from 'components/Advantages/Advantages';
 import { HeroSection } from 'components/HeroSection/HeroSection';
-import { OurService } from 'components/OurService/OurService';
 import { PopularDestination } from 'components/PopularDestination/PopularDestination';
 import { SelfBook } from 'components/SelfBook/SelfBook';
-import { Container } from 'components/StyledComponents/Container';
 
 import heroBackgroundImage from '../../assets/images/heroImage.jpeg';
 
@@ -11,9 +9,6 @@ const HomePage = () => {
   return (
     <>
       <HeroSection backgroundImage={heroBackgroundImage} />
-      <Container>
-        <OurService />
-      </Container>
       <PopularDestination />
       <SelfBook />
       <Advantages />
